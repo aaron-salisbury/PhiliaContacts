e0378da1a420f2401822f56dffd4da43994bb85e
Aaron Salisbury <aaron.salisbury@live.com>
2021-04-04 10:50:45 -0500

    Complete export writer.

--- PhiliaContacts/PhiliaContacts.Core/Writer.cs
+++ PhiliaContacts/PhiliaContacts.Core/Writer.cs
@@ -16,8 +16,6 @@ namespace PhiliaContacts.Core
             {
                 manager.Logger.Information("Beginning to write contacts.");
 
-                string rev = "REV:" + "2021-03-26T17:56:19Z"; //TODO: get this format from DateTime.Now.
-
                 StringBuilder stringBuilder = new StringBuilder();
                 int itemCounter = 0;
 
@@ -28,93 +26,95 @@ namespace PhiliaContacts.Core
                         continue;
                     }
 
-                    //TODO: Limit line length. Wrap and indent one space.
+                    stringBuilder.AppendLine(WrapLine(HEADER + Environment.NewLine + VERSION));
 
-                    stringBuilder.AppendLine(HEADER + Environment.NewLine + VERSION);
-
-                    stringBuilder.AppendLine($"N:{contact.FamilyName};{contact.GivenName};{contact.MiddleName};;");
+                    stringBuilder.AppendLine(WrapLine($"N:{contact.FamilyName};{contact.GivenName};{contact.MiddleName};;"));
 
                     if (!string.IsNullOrEmpty(contact.FormattedName))
                     {
-                        stringBuilder.AppendLine($"FN:{contact.FormattedName}");
+                        stringBuilder.AppendLine(WrapLine($"FN:{contact.FormattedName}"));
                     }
 
                     if (!string.IsNullOrEmpty(contact.Nickname))
                     {
-                        stringBuilder.AppendLine($"NICKNAME:{contact.Nickname}");
+                        stringBuilder.AppendLine(WrapLine($"NICKNAME:{contact.Nickname}"));
                     }
 
                     if (contact.IsFavorite)
                     {
-                        stringBuilder.AppendLine("CATEGORIES:starred");
+                        stringBuilder.AppendLine(WrapLine("CATEGORIES:starred"));
                     }
 
                     if (contact.Birthday != null)
                     {
-                        stringBuilder.AppendLine($"BDAY;VALUE=date:{contact.Birthday.Value:yyyy-MM-dd}");
+                        stringBuilder.AppendLine(WrapLine($"BDAY;VALUE=date:{contact.Birthday.Value:yyyy-MM-dd}"));
                     }
 
                     if (!string.IsNullOrEmpty(contact.Organization))
                     {
-                        stringBuilder.AppendLine($"ORG:{contact.Organization};");
+                        stringBuilder.AppendLine(WrapLine($"ORG:{contact.Organization};"));
                     }
 
                     if (!string.IsNullOrEmpty(contact.Title))
                     {
-                        stringBuilder.AppendLine($"TITLE:{contact.Title}");
+                        stringBuilder.AppendLine(WrapLine($"TITLE:{contact.Title}"));
                     }
 
                     if (!string.IsNullOrEmpty(contact.Url))
                     {
-                        stringBuilder.AppendLine($"URL;TYPE=HOME:{contact.Url}");
+                        stringBuilder.AppendLine(WrapLine($"URL;TYPE=HOME:{contact.Url}"));
                     }
 
                     if (!string.IsNullOrEmpty(contact.Notes))
                     {
-                        stringBuilder.AppendLine($"NOTE:{contact.Notes}");
+                        stringBuilder.AppendLine(WrapLine($"NOTE:{contact.Notes}"));
                     }
 
                     foreach(EmailAddress email in contact.EmailAddresses)
                     {
                         if (email.Type == EmailAddress.EmailType.None)
                         {
-                            stringBuilder.AppendLine($"EMAIL;TYPE=INTERNET:{email.Email}");
+                            stringBuilder.AppendLine(WrapLine($"EMAIL;TYPE=INTERNET:{email.Email}"));
                         }
                         else
                         {
-                            stringBuilder.AppendLine($"EMAIL;TYPE=INTERNET;TYPE={email.Type.ToString().ToUpper()}:{email.Email}");
+                            stringBuilder.AppendLine(WrapLine($"EMAIL;TYPE=INTERNET;TYPE={email.Type.ToString().ToUpper()}:{email.Email}"));
                         }
                     }
 
-                    //TODO: Address, phones.
+                    foreach(PhoneNumber phone in contact.PhoneNumbers)
+                    {
+                        itemCounter++;
+                        stringBuilder.AppendLine(WrapLine($"item{itemCounter}.TEL:{phone.Number}"));
+                        stringBuilder.AppendLine(WrapLine($"item{itemCounter}.X-ABLabel:{phone.Type}"));
+                    }
+
+                    if (!string.IsNullOrEmpty(contact.Street))
+                    {
+                        stringBuilder.AppendLine(WrapLine($"ADR;TYPE={contact.AddressType.ToString().ToUpper()}:;;{contact.Street};{contact.City};{contact.State};{contact.Zip};{contact.CountryRegion}"));
+                    }
 
                     if (!string.IsNullOrEmpty(contact.TwitterUser))
                     {
-                        stringBuilder.AppendLine($"X-SOCIALPROFILE;X-USER={contact.TwitterUser};TYPE=twitter:http://twitter.com/" + contact.TwitterUser);
+                        stringBuilder.AppendLine(WrapLine($"X-SOCIALPROFILE;X-USER={contact.TwitterUser};TYPE=twitter:http://twitter.com/" + contact.TwitterUser));
                     }
 
                     if (!string.IsNullOrEmpty(contact.FacebookUser))
                     {
-                        stringBuilder.AppendLine($"X-SOCIALPROFILE;X-USER={contact.FacebookUser};TYPE=facebook:http://facebook.com/" + contact.FacebookUser);
+                        stringBuilder.AppendLine(WrapLine($"X-SOCIALPROFILE;X-USER={contact.FacebookUser};TYPE=facebook:http://facebook.com/" + contact.FacebookUser));
                     }
 
                     if (!string.IsNullOrEmpty(contact.LinkedInUser))
                     {
-                        stringBuilder.AppendLine($"X-SOCIALPROFILE;X-USER={contact.LinkedInUser};TYPE=linkedin:http://www.linkedin.com/in/" + contact.LinkedInUser);
+                        stringBuilder.AppendLine(WrapLine($"X-SOCIALPROFILE;X-USER={contact.LinkedInUser};TYPE=linkedin:http://www.linkedin.com/in/" + contact.LinkedInUser));
                     }
 
                     if (contact.Photo != null && contact.Photo != manager.DefaultContactImage)
                     {
-                        //stringBuilder.AppendLine($"PHOTO:TYPE=JPEG;ENCODING=BASE64:{Convert.ToBase64String(contact.Photo, 0, contact.Photo.Length)}");
-
-                        //stringBuilder.AppendLine("PHOTO;ENCODING=BASE64;TYPE=JPEG:");
-                        //stringBuilder.AppendLine(Convert.ToBase64String(contact.Photo));
-                        //stringBuilder.AppendLine(string.Empty);
-
-                        stringBuilder.AppendLine($"PHOTO:TYPE=JPEG;ENCODING=BASE64:{Convert.ToBase64String(contact.Photo)}");
+                        stringBuilder.AppendLine(WrapLine($"PHOTO:TYPE=JPEG;ENCODING=BASE64:{Convert.ToBase64String(contact.Photo)}"));
                     }
 
-                    stringBuilder.AppendLine(rev + Environment.NewLine + FOOTER);
+                    stringBuilder.AppendLine(WrapLine(FOOTER));
                 }
 
                 if (stringBuilder.Length > 0)
@@ -133,6 +133,35 @@ namespace PhiliaContacts.Core
                 manager.Logger.Error($"Failed to write contacts: {e.Message}");
                 return null;
             }
+        }
+
+        private static string WrapLine(string line)
+        {
+            const int MAX_LENGTH = 75;
+
+            StringBuilder builder = new StringBuilder();
+
+            int counter = 0;
+            foreach(char c in line)
+            {
+                counter++;
+
+                if (counter < MAX_LENGTH)
+                {
+                    builder.Append(c);
+                }
+                else if (counter == MAX_LENGTH)
+                {
+                    builder.AppendLine(c.ToString());
+                }
+                else if (counter > MAX_LENGTH)
+                {
+                    builder.Append(" " + c);
+                    counter = 2;
+                }
+            }
+
+            return builder.ToString();
         }
     }
 }
